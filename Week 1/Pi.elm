71fee16a73b73bbc078c81c2dd70cc0dc6abc6ad
lasagnaphil <lasagnaphil@snu.ac.kr>
2015-09-14 02:10:49 +0900

    Second commit

--- Week 1/Pi.elm
+++ Week 1/Pi.elm
@@ -3,8 +3,13 @@ module Pi where
 import Signal exposing ((<~), (~))
 import Window
 import Random
-import Time exposing ()
-import List exposing ((::), fst, snd)
+import Random.Extra
+import Time
+import List exposing ((::))
+
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
 
 type alias Point = { x:Float, y:Float }
 type alias State = ((Int, List Point), (Int, List Point))
@@ -13,45 +18,52 @@ type alias State = ((Int, List Point), (Int, List Point))
 upstate : Point -> State -> State
 upstate point state =
     if (point.x)^2 + (point.y)^2 < 100.0
-        then ((state |> fst |> fst + 1, (point.x, point.y) :: state |> fst |> snd),
-            (state |> snd))
-        else ((state |> fst),
-            (state |> snd |> fst + 1, (point.x, point.y) :: state |> snd |> snd))
+        then ((fst (fst state) + 1, point :: (snd (fst state))),
+            (snd state))
+        else ((fst state),
+            (fst (snd state) + 1, point :: (snd (snd state))))
 
 initState : State
 initState = ((0,[]),(0,[]))
 
 -- the controller
-randPointGen : Generator (Float, Float)
-randPointGen =
-    pair (float -100, 100) (float -100 100)
+floatGen : Random.Generator Float
+floatGen =
+    Random.float -100 100
+
+
+randPointGen : Random.Generator Point
+randPointGen generator =
+    Random.Extra.map Point floatGen floatGen
 
 genPoint : Random.Seed -> (Point, Random.Seed)
 genPoint seed =
-    generate randPointGen seed
+    Random.generate randPointGen seed
 
 signalPointSeed : Signal (Point, Random.Seed)
 signalPointSeed =
-    genPoint <~ (Signal.foldp genPoint initial)
-    --({x=0, y=0}, Random.initialSeed 17)
-
+    Signal.foldp genPoint ({x=0, y=0}, Random.initialSeed 17) (Time.every Time.second)
 
 signalPoint : Signal Point
 signalPoint =
-    Time.every Time.second
-
+    fst <~ signalPointSeed
 
 -- the view
 view : (Int, Int) -> State -> Element
 view w h state =
-    pointsToCircles <
+    collage w h [
+        pointsToCircles (rgb 255 0 0) (state|>fst|>snd),
+        pointsToCircles (rgb 0 255 0) (state|>snd|>snd),
+        toForm (show piApprox state)
+    ]
 
-pointsToCircles : Color.Color -> List.Point -> List Form
+pointsToCircles : Color.Color -> List Point -> List Form
 pointsToCircles color pointlst =
+    List.map (\x -> move x (color (circle 5))) pointlst 
 
 piApprox : State -> Float
-piApprox =
-    4 * (state|>fst|>fst / (state|>fst|>fst + state|>snd|>fst)
+piApprox state =
+    4 * (state|>fst|>fst) / (state|>fst|>fst + state|>snd|>fst)
 
 main : Signal Element
 main =
